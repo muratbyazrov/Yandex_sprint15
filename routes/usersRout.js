d3a4a81a8bd405247f4dbbe9cf35c2dcdf443310
Murat Byazrov <murat.byazrov@yandex.ru>
2020-06-19 07:43:50 +0300

    Добавили валидатор методу поиска пользователя по id

--- routes/usersRout.js
+++ routes/usersRout.js
@@ -3,6 +3,8 @@ const usersRouter = require('express').Router();
 
 // подключим предварительную валидацию с помощью библиотек
 const { celebrate, Joi } = require('celebrate');
+// подключили валидацию id. Пришлось ставить доп. модуль https://www.npmjs.com/package/joi-objectid
+Joi.objectId = require('joi-objectid')(Joi);
 
 // подключили мидлвер для авторизации
 const auth = require('../middlewares/auth');
@@ -15,7 +17,11 @@ const {
 // применяем нужные обработчики при соответсвующих запросах
 // auth - это мидлвер для авторизации. После неё идут роуты, кторые нужно авторизовывать
 usersRouter.get('/', auth, getUsers);
-usersRouter.get('/:id', auth, getUserById);
+usersRouter.get('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.objectId(),
+  }),
+}), auth, getUserById);
 usersRouter.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
